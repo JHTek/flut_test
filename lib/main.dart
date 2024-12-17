960ec34119a0e7d8a5da700971251b3a56da2a21
JHTek <jht020306@naver.com>
2024-12-17 17:11:07 +0900

    Final Demo

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginPage(),
         '/register': (context) => RegisterPage(),
-        '/calendar': (context) => CalendarPage(), // 캘린더 페이지 경로 추가
       },
     );
   }
@@ -32,24 +31,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('메인 페이지'),
+        title: Text('TaskFlow'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Text(
+              'Welcome to TaskFlow!',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo,
+              ),
+            ),
+            SizedBox(height: 30),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/login');
               },
-              child: Text('로그인 페이지로 이동'),
+              child: Text('로그인 으로 이동'),
             ),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/register');
               },
-              child: Text('회원가입 페이지로 이동'),
+              child: Text('회원가입 으로 이동'),
             ),
           ],
         ),
